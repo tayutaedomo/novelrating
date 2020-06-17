53895420a99628ec5faa884e0c07fa82bb32cfb7
tayutaedomo <tayutaedomo@gmail.com>
2020-06-17 21:14:09 +0900

    Refactoring of crawler

--- scripts/scraping/narou_page.py
+++ scripts/scraping/narou_page.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import datetime
-import time
 import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -9,61 +8,9 @@ from selenium.webdriver.chrome.options import Options
 ROOT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
 DEST_ROOT_PATH = os.path.join(ROOT_PATH, 'data', 'narou')
 
+sys.path.append(ROOT_PATH)
 
-def get_novel(driver, ncode, page):
-    url = 'https://ncode.syosetu.com/{}/{}/'.format(ncode, page)
-    print(datetime.datetime.now().isoformat(), 'GET:', url)
-
-    novel = {
-        'url': url,
-        'title': '',
-        'subtitle': '',
-        'body': '',
-        'downloaded': False,
-    }
-
-    driver.get(url)
-
-    now_str = datetime.datetime.now().isoformat()
-
-    try:
-        title_elem = driver.find_element_by_css_selector('div.contents1 a')
-        novel['title'] = title_elem.text
-
-        subtitle_elem = driver.find_element_by_class_name('novel_subtitle')
-        novel['subtitle'] = subtitle_elem.text
-
-        body_elem = driver.find_element_by_css_selector('div#novel_honbun')
-        novel['body'] = body_elem.text
-
-        novel['time'] = now_str
-        novel['downloaded'] = True
-
-    except Exception as e:
-        print(datetime.datetime.now().isoformat(), e, novel['url'])
-
-    return novel
-
-
-def write_novel_to_txt(ncode, page, novel):
-    file_name = '{}-p{}.txt'.format(ncode, page)
-    dest_path = os.path.join(DEST_ROOT_PATH, file_name)
-
-    if os.path.exists(dest_path):
-        print(datetime.datetime.now().isoformat(), 'Skip:', ncode, page)
-        return False
-
-    lines = [
-        novel['url'],
-        repr(novel['title']),
-        repr(novel['subtitle']),
-        repr(novel['body']),
-    ]
-
-    with open(dest_path, 'w') as f:
-        f.write('\n'.join(lines))
-
-    return True
+from scripts.utils.novel import NarouPageCrawler
 
 
 if __name__ == '__main__':
@@ -95,18 +42,8 @@ if __name__ == '__main__':
     options.add_argument('--incognito')
     driver = webdriver.Chrome(options=options)
 
-    for page in range(page_start, page_end + 1):
-        novel = get_novel(driver, ncode, page)
-
-        if not novel['downloaded']:
-            print(datetime.datetime.now().isoformat(), 'Failed:', ncode, page)
-        else:
-            if write_novel_to_txt(ncode, page, novel):
-                print(datetime.datetime.now().isoformat(), 'New:', ncode, page)
-
-        if page < page_end:
-            print(datetime.datetime.now().isoformat(), 'Sleep(3)')
-            time.sleep(3)
+    crawler = NarouPageCrawler(driver, DEST_ROOT_PATH, 3)
+    crawler.crawl(ncode, page_start, page_end)
 
     # Capture
     #file_path = os.path.join(ROOT_PATH, 'tmp', 'capture.png')

04c6cc21972c85350e09f6d729250b7873a31430
tayutaedomo <tayutaedomo@gmail.com>
2020-06-28 00:51:33 +0900

    Fix scraping/narou_rating.py

--- scripts/scraping/narou_rating.py
+++ scripts/scraping/narou_rating.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 
 ROOT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
 BOOKMARK_CSV_PATH = os.path.join(ROOT_PATH, 'data', 'narou', 'my_bookmark.csv')
+BOOKMARK_RATING_CSV_PATH = os.path.join(ROOT_PATH, 'data', 'narou', 'my_bookmark_rating.csv')
 
 sys.path.append(ROOT_PATH)
 
@@ -30,6 +31,19 @@ def load_bookmark_csv():
             })
 
     return bookmarks
+
+
+def create_cache():
+    cache = {}
+
+    if not os.path.exists(BOOKMARK_RATING_CSV_PATH):
+        return cache
+
+    with open(BOOKMARK_RATING_CSV_PATH, 'r', encoding='utf-8') as f:
+        for row in csv.reader(f):
+            cache[row[0]] = row[2]  # { ncode: rating, ... }
+
+    return cache
 
 
 def get_rating(driver, ncode):
@@ -61,9 +75,24 @@ def get_rating(driver, ncode):
     return rating
 
 
+def append_to_csv(bookmark):
+    csv_line = '"{}","{}","{}","{}"'.format(
+        bookmark.get('ncode'),
+        bookmark.get('category'),
+        bookmark.get('rating'),
+        bookmark.get('title'),
+    )
+
+    with open(BOOKMARK_RATING_CSV_PATH, 'a') as f:
+        f.write(csv_line + '\n')
+
+
 if __name__ == '__main__':
     bookmarks = load_bookmark_csv()
     print(datetime.datetime.now().isoformat(), 'Bookmark Count:', len(bookmarks))
+
+    cache = create_cache()
+    print(datetime.datetime.now().isoformat(), 'Cache Count:', len(cache))
 
     email = os.getenv('NAROU_EMAIL')
     password = os.getenv('NAROU_PASSWORED')
@@ -75,11 +104,27 @@ if __name__ == '__main__':
 
     login_narou(driver, email, password)
 
-    #ncode = 'n7951ei'
-    #ncode = 'n7787eq'
-    ncode = 'n6859du'
-    rating = get_rating(driver, ncode)
-    print(rating)
+    for i, bookmark in enumerate(bookmarks[:50]):
+        #ncode = 'n7951ei'  # No Rating
+        #ncode = 'n6859du'  # is-half
+        ncode = bookmark['ncode']
+
+        if cache.get(ncode):
+            print(datetime.datetime.now().isoformat(), i, ncode, ', Skip')
+            continue
+
+        rating = get_rating(driver, ncode)
+
+        if rating is None:
+            print(datetime.datetime.now().isoformat(), i, ncode, ', No Rating')
+        else:
+            print(datetime.datetime.now().isoformat(), i, ncode, ', Rating:', rating)
+            bookmark['rating'] = rating
+            append_to_csv(bookmark)
+
+        if i < len(bookmarks) - 1:
+            print(datetime.datetime.now().isoformat(), 'Sleep(3)')
+            time.sleep(3)
 
     # Capture
     #file_path = os.path.join(ROOT_PATH, 'tmp', 'capture.png')

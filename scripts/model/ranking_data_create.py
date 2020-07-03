9db7878fdc91d72384ac82fae780a89c863e255f
tayutaedomo <tayutaedomo@gmail.com>
2020-07-03 21:32:03 +0900

    Add model/ranking_data_create.py

---
+++ scripts/model/ranking_data_create.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import datetime
+import pprint as pp
+
+ROOT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
+
+sys.path.append(ROOT_PATH)
+
+from scripts.utils.novel import load_ranking_csv
+from scripts.utils.model import RankingDataMaker
+
+
+if __name__ == '__main__':
+    mode = None
+    ncode_list = []
+
+    if len(sys.argv) > 1:
+        mode = sys.argv[1]
+
+    if mode == 'ncode':
+        ncode = sys.argv[2]
+        ncode_list.append(ncode)
+
+    else:
+        ncode_list = [ranking['ncode'] for ranking in load_ranking_csv()]
+        print(datetime.datetime.now().isoformat(), 'Ranking Count:', len(ncode_list))
+
+    maker = RankingDataMaker()
+    maker.load(ncode_list)
+
+    pp.pprint(maker.unique_keywords.get_sorted_items(True))
+    pp.pprint(maker.unique_word_classes.get_sorted_items(True))
+    pp.pprint(maker.unique_keywords.get_unique_keys())
+    pp.pprint(maker.unique_word_classes.get_unique_keys())
+    print(len(ncode_list), len(maker.inputs_list))
+
+    #maker.make()
+    #maker.save()
+
